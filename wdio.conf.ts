a04c42e04b499b317c6282d203c24e079a3a6f63
Venu Abbagoni <venu.abbagonii@gmail.com>
2024-11-27 14:42:45 +0000

    Updated table selection feature file

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -135,7 +135,7 @@ export const config: WebdriverIO.Config = {
     // If you are using Cucumber you need to specify the location of your step definitions.
     cucumberOpts: {
         // <string[]> (file/dir) require files before executing features
-        require: ['./test/features/step-definitions/steps.ts'],
+        require: ['./test/step-definitions/*.ts'],
         // <boolean> show full backtrace for errors
         backtrace: false,
         // <string[]> ("extension:module") require files with the given EXTENSION after requiring MODULE (repeatable)
